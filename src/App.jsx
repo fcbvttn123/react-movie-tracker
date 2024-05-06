f74f59032ca553e0e964388ea14117148d5fa486
ngan <vutran@sheridancollege.ca>
2024-05-06 08:22:11 -0400

    Folder Structure and Router Installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import { Login } from "./pages/auth/Login"
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline"> Hello world! </h1>
+    <Login />
   )
 }
 
---
+++ src/pages/auth/Login.jsx
@@ -0,0 +1,5 @@
+export function Login() {
+    return (
+        <button className="m-4 px-8 py-3 bg-slate-400 rounded-xl">Login Using Google Account</button>
+    )
+}
