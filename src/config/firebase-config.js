75cb1b67132aaaf0d47c7afd6c2d0510b32f21eb
ngan <vutran@sheridancollege.ca>
2024-05-06 08:12:38 -0400

    Firebase Setup

---
+++ src/config/firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA3u1UIt4vX-0d4XE8S1SkW_dFUEE4x3Cg",
+  authDomain: "react-movie-tracker-f6549.firebaseapp.com",
+  projectId: "react-movie-tracker-f6549",
+  storageBucket: "react-movie-tracker-f6549.appspot.com",
+  messagingSenderId: "997533798549",
+  appId: "1:997533798549:web:a528db7ec96357823d8b9c"
+};
+
+const app = initializeApp(firebaseConfig);
