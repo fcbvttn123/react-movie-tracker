860f31bba5e9ce4b8e9abb99663a46f79ec9b253
ngan <vutran@sheridancollege.ca>
2024-05-11 10:21:49 -0400

    Grid is added to MyList component for RWD

--- src/pages/home/MyList.jsx
+++ src/pages/home/MyList.jsx
@@ -11,13 +11,13 @@ export function MyList() {
         let res = await useGetMyList(useGetAccountInfo().email)
         setList(res)
     }
-    
+
     useEffect(() => {
         getMyList()
     }, [])
 
     return (
-        <div>
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
             {list && list.map(e => <MovieCards key={e.movieId} id={e.movieId} imgName={e.imgName} title={e.title} renderedInMyList={true} rowId={e.id} />)}
         </div>
     )
